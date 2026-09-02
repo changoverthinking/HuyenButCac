b204bb577e64f3616e06223a2dae7ff616d30b75
duahauDevSolo <nductienth36@gmail.com>
2026-09-02 11:31:59 +0900

    Add files via upload

--- src/app/appConfig.ts
+++ src/app/appConfig.ts
@@ -5,7 +5,7 @@ export const APP_CONFIG = {
   appNameEn: "Mystic Notes",
   appId: "com.huyenbut.notes",
   repository: "HuyenButCac",
-  version: "0.16.0-ui-v2",
+  version: "0.16.1-ui-hotfix",
 };
 
 export const NAME_CANDIDATES: { name: string; meaning: string }[] = [
