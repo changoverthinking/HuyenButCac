9cc94c1a5ccdf72f1c402a595fd35f7850f7ef99
duahauDevSolo <nductienth36@gmail.com>
2026-09-02 18:37:33 +0900

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './themes/festival-themes.css'
 import './app-enhancements.css'
+import './features/tieu-nhi/tieu-nhi.css'
 import App from './App.tsx'
+import { TieuNhiLauncher } from './features/tieu-nhi/TieuNhiLauncher.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <TieuNhiLauncher />
   </StrictMode>,
 )
